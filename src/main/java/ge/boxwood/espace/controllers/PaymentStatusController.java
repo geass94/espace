7b5a32889da88a101ed2a10b5431e5ce43ec2bc3
geass94 <okoridze@yahoo.com>
2018-06-28 16:44:18 +0400

    charger services with orders

--- src/main/java/ge/boxwood/espace/controllers/PaymentStatusController.java
+++ src/main/java/ge/boxwood/espace/controllers/PaymentStatusController.java
@@ -2,16 +2,17 @@ package ge.boxwood.espace.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PaymentStatusController {
     @GetMapping("/checkoutComplete")
-    public String getCheckoutComplete(){
-        return "checkoutComplete";
+    public ModelAndView getCheckoutComplete(){
+        return new ModelAndView("checkoutComplete");
     }
 
     @GetMapping("/checkoutFailed")
-    public String getCheckoutFailed(){
-        return "checkoutFailed";
+    public ModelAndView getCheckoutFailed(){
+        return new ModelAndView("checkoutFailed");
     }
 }
