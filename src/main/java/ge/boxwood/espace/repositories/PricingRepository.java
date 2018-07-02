1f22a32735d7a348ffc5c36ef14f7665f1a5cac9
geass94 <okoridze@yahoo.com>
2018-07-02 17:22:02 +0400

    charger services with orders

--- src/main/java/ge/boxwood/espace/repositories/PricingRepository.java
+++ src/main/java/ge/boxwood/espace/repositories/PricingRepository.java
@@ -3,10 +3,11 @@ package ge.boxwood.espace.repositories;
 import ge.boxwood.espace.models.Pricing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PricingRepository extends JpaRepository<Pricing, Long> {
-    @Query(value = "select * from espace.pricing_list AS p where p.range_start<= ?1 AND p.range_end>= ?1 ORDER BY id DESC LIMIT 1;", nativeQuery = true)
-    Pricing findPriceWithinRanges(double start);
+    @Query(value = "select p from pricing_list AS p where p.range_start<= :val AND p.range_end>= :val ORDER BY id DESC LIMIT 1;", nativeQuery = true)
+    Pricing findPriceWithinRanges(@Param("val") double val);
 }
