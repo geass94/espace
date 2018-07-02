2d6b340c15d8e581f7e50feb44ae9cb8b3417dd7
geass94 <okoridze@yahoo.com>
2018-07-02 13:09:59 +0400

    charger services with orders

--- src/main/java/ge/boxwood/espace/services/impl/ChargerServiceImpl.java
+++ src/main/java/ge/boxwood/espace/services/impl/ChargerServiceImpl.java
@@ -140,7 +140,6 @@ public class ChargerServiceImpl implements ChargerService {
     public ChargerInfoDTO start(Long cID, Long conID, Long cardID) {
         Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
         String username = currentUser.getName();
-        ChargerInfoDTO dto = new ChargerInfoDTO();
         User user = userService.getByUsername(username);
         Order order = orderRepository.findByChargerAndUserAndConfirmed(chargerRepository.findByChargerId(cID), user, false);
         Charger charger = this.info(cID);
@@ -150,48 +149,28 @@ public class ChargerServiceImpl implements ChargerService {
                 ChargerInfo chargerInfo = new ChargerInfo();
                 JSONObject chargerStart = chargerRequestUtils.start(cID, conID);
                 chargerInfo.setChargerTransactionId(chargerStart.get("data") != null && !chargerStart.get("data").equals(null) ? chargerStart.get("data").toString() : "0");
-                JSONObject transactionInfo = chargerRequestUtils.transaction(Long.valueOf(chargerInfo.getChargerTransactionId()));
-                JSONObject transaction = transactionInfo.getJSONObject("data");
                 chargerInfo.setCharger(charger);
-                chargerInfo.setTransStart(transaction.get("transStart") != null && !transaction.get("transStart").equals(null) ? (long) transaction.get("transStart") : 0L);
-                chargerInfo.setTransStop(transaction.get("transStop") != null && !transaction.get("transStop").equals(null) ? (long)transaction.get("transStop") : 0L);
-                chargerInfo.setMeterStart(transaction.get("meterStart") != null && !transaction.get("meterStart").equals(null) ? (long)(int)transaction.get("meterStart") : 0L);
-                chargerInfo.setMeterStop(transaction.get("meterStop") != null && !transaction.get("meterStop").equals(null) ? (long)(int)transaction.get("meterStop") : 0L);
-                chargerInfo.setChargingPower(transaction.get("kiloWattHour") != null && !transaction.get("kiloWattHour").equals(null) ? Double.valueOf(transaction.get("kiloWattHour").toString()) : 0d);
-                chargerInfo.setChargeTime(transaction.get("chargingTime") != null && !transaction.get("chargingTime").equals(null) ? (long)(int)transaction.get("chargingTime") : 0L);
-                chargerInfo.setStartUUID(transaction.get("uuidStart") != null && !transaction.get("uuidStart").equals(null) ? transaction.get("uuidStart").toString() : "");
-                chargerInfo.setStopUUID(transaction.get("uuidEnd") != null && !transaction.get("uuidEnd").equals(null) ? transaction.get("uuidEnd").toString() : "");
                 chargerInfo.setResponseCode((Integer) chargerStart.get("responseCode"));
-                chargerInfo.setConsumedPower(transaction.get("consumed") != null && !transaction.get("consumed").equals(null) ? Long.valueOf(transaction.get("uuidEnd").toString()) : 0L);
+
                 if(chargerInfo.getResponseCode() >= 200 && chargerInfo.getResponseCode() < 250){
                     Order newOrder = new Order(user);
                     newOrder.setPaymentType(PaymentType.CREDITCARD);
                     newOrder.setCharger(chargerInfo.getCharger());
                     newOrder.setChargerTransactionId(Long.valueOf(chargerInfo.getChargerTransactionId()));
-                    chargerInfo.setOrder(newOrder);
                     newOrder = orderRepository.save(newOrder);
+                    chargerInfo.setOrder(newOrder);
                     Payment payment = new Payment(0f, newOrder);
                     if(cardID != null && cardID > 0){
                         CreditCard creditCard = creditCardRepository.findOne(cardID);
                         payment.setCreditCard(creditCard);
                     }
-
                     payment = paymentRepository.save(payment);
                     newOrder.setPayments(Collections.singletonList(payment));
                     paymentRepository.flush();
                     orderRepository.flush();
-
-                    dto.setChargerId(chargerInfo.getCharger().getChargerId());
-                    dto.setChargePower(chargerInfo.getChargingPower());
-                    dto.setChargeTime(chargerInfo.getChargeTime());
-                    dto.setChargerStatus(chargerInfo.getCharger().getStatus());
-                    dto.setChargerType(chargerInfo.getCharger().getType());
-                    dto.setOrderUUID(chargerInfo.getOrder().getUuid());
-                    dto.setPaymentUUID(chargerInfo.getOrder().getPayments().get(0).getUuid());
-                    dto.setChargerTrId(chargerInfo.getChargerTransactionId());
-                    dto.setChargingFinished(false);
-                    dto.setConsumedPower(chargerInfo.getConsumedPower());
+                    ChargerInfoDTO dto = this.transaction(Long.valueOf(chargerInfo.getChargerTransactionId()));
                     charger.setStatus(1);
+                    dto.setChargerStatus(charger.getStatus());
                     chargerRepository.save(charger);
                     return dto;
                 }else
