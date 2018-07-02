6d10549c1c0fd7a4541dea0a08ea440e67abb894
geass94 <okoridze@yahoo.com>
2018-07-02 16:37:33 +0400

    charger services with orders

--- src/main/java/ge/boxwood/espace/services/impl/PricingServiceImpl.java
+++ src/main/java/ge/boxwood/espace/services/impl/PricingServiceImpl.java
@@ -12,7 +12,7 @@ public class PricingServiceImpl implements PricingService {
     private PricingRepository pricingRepository;
     @Override
     public Float getPriceForChargingPower(Double chargingPower) {
-        Pricing pricing = pricingRepository.findDistinctTopByRangeStartIsGreaterThanEqualAndRangeEndIsLessThanEqual(chargingPower, chargingPower);
+        Pricing pricing = pricingRepository.findDistinctFirstByRangeStartIsGreaterThanEqualAndRangeEndIsLessThanEqual(chargingPower, chargingPower);
         return pricing.getPrice();
     }
 }
