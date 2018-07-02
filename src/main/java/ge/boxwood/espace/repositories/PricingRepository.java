ffaa23f3b9bd8b4d594220261d62c048580763c9
geass94 <okoridze@yahoo.com>
2018-07-02 17:06:45 +0400

    charger services with orders

--- src/main/java/ge/boxwood/espace/repositories/PricingRepository.java
+++ src/main/java/ge/boxwood/espace/repositories/PricingRepository.java
@@ -2,9 +2,10 @@ package ge.boxwood.espace.repositories;
 
 import ge.boxwood.espace.models.Pricing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PricingRepository extends JpaRepository<Pricing, Long> {
-    Pricing findDistinctFirstByRangeStartIsLessThanEqualAndRangeEndIsGreaterThanEqual(double start, double end);
+    Pricing findDistinctFirstByRangeStartIsGreaterThanEqualAndRangeEndIsLessThanEqual(double start, double end);
 }
