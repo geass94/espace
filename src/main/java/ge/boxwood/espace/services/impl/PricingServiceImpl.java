b4179d83627f9479c3b687892dcd04b6c8a7df61
geass94 <okoridze@yahoo.com>
2018-07-02 17:28:31 +0400

    charger services with orders

--- src/main/java/ge/boxwood/espace/services/impl/PricingServiceImpl.java
+++ src/main/java/ge/boxwood/espace/services/impl/PricingServiceImpl.java
@@ -6,13 +6,20 @@ import ge.boxwood.espace.services.PricingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 @Service
 public class PricingServiceImpl implements PricingService {
     @Autowired
     private PricingRepository pricingRepository;
+    @Autowired
+    private EntityManager em;
     @Override
     public Float getPriceForChargingPower(Double chargingPower) {
-        Pricing pricing = pricingRepository.findPriceWithinRanges(chargingPower);
-        return pricing.getPrice();
+        Query q = em.createNativeQuery("select * from espace.pricing_list AS p where p.range_start<= :power AND p.range_end>= :power ORDER BY id DESC LIMIT 1;", Pricing.class);
+        q.setParameter("power", chargingPower);
+        Pricing p = (Pricing) q.getSingleResult();
+        return p.getPrice();
     }
 }
