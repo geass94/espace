d79dda0678b0c0f1c5ae613347243d48b8efbc7a
geass94 <okoridze@yahoo.com>
2018-07-19 11:02:00 +0400

    charger services with orders

---
+++ src/main/kotlin/ge/boxwood/espace/controllers/AppController.kt
@@ -0,0 +1,15 @@
+package ge.boxwood.espace.controllers
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AppController {
+
+    @GetMapping("")
+    fun checkAvailability(): ResponseEntity<Any>{
+
+        return ResponseEntity.ok().build()
+    }
+}
